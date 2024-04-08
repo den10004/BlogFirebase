deb12e565ca24d0780c0d7fff6639ce1f52fa793
Denis <denis-sergeev-85@inbox.ru>
2024-04-08 17:15:18 +0300

    исправлены цвета

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,15 +1,54 @@
 import { NavLink } from "react-router-dom";
 
 const Category = ({ uniqCat }) => {
-  // console.log(uniqCat);
   return (
     <>
       <NavLink to={`/category/${uniqCat.category}`}>
-        <div className="row pb-4">
-          <span className="category catg-color bgc4287f5">
-            {uniqCat.category}
-          </span>
-        </div>
+        <span>
+          {uniqCat.category === "Путешевствия" && (
+            <h6
+              className="category catg-color bgc4287f5"
+              style={{ marginRight: "10px" }}
+            >
+              {uniqCat.category}
+            </h6>
+          )}
+          {uniqCat.category === "Технологии" && (
+            <h6
+              className="category catg-color bgcc43421"
+              style={{ marginRight: "10px" }}
+            >
+              {uniqCat.category}
+            </h6>
+          )}
+          {uniqCat.category === "Новости" && (
+            <h6
+              className="category catg-color bgc32CD32"
+              style={{ marginRight: "10px" }}
+            >
+              {uniqCat.category}
+            </h6>
+          )}
+          {uniqCat.category === "Бизнес" && (
+            <h6
+              className="category catg-color bgcc808000"
+              style={{ marginRight: "10px" }}
+            >
+              {uniqCat.category}
+            </h6>
+          )}
+          {uniqCat.category !== "Путешевствия" &&
+            uniqCat.category !== "Технологии" &&
+            uniqCat.category !== "Бизнес" &&
+            uniqCat.category !== "Новости" && (
+              <h6
+                className="category catg-color bgc000000"
+                style={{ marginRight: "10px" }}
+              >
+                {uniqCat.category}
+              </h6>
+            )}
+        </span>
       </NavLink>
     </>
   );
