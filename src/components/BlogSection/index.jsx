666d5d378adfe8dd80ee1bd34749865edb610431
Denis <denis-sergeev-85@inbox.ru>
2024-04-08 11:10:46 +0300

    исправлены ссылки

--- src/components/BlogSection/index.jsx
+++ src/components/BlogSection/index.jsx
@@ -16,82 +16,83 @@ const BlogSection = ({
   handleDelete,
 }) => {
   return (
-    <div className="row pb-4" key={id}>
-      <div className="col-md-5">
-        <div className="hover-blogs-img">
-          <div className="blogs-img">
-            {imgUrl ? (
-              <img src={imgUrl} alt={title} />
-            ) : (
-              <div
-                style={{
-                  backgroundColor: "gray",
-                  height: "100%",
-                  opacity: "0.4",
-                  padding: "10px",
-                }}
-              >
-                <h3>Нет изображения</h3>
+    <>
+      <Link to={`/detail/${id}`}>
+        <div className="row pb-4" key={id}>
+          <div className="col-md-5">
+            <div className="hover-blogs-img">
+              <div className="blogs-img">
+                {imgUrl ? (
+                  <img src={imgUrl} alt={title} />
+                ) : (
+                  <div
+                    style={{
+                      backgroundColor: "gray",
+                      height: "100%",
+                      opacity: "0.4",
+                      padding: "10px",
+                    }}
+                  >
+                    <h3>Нет изображения</h3>
+                  </div>
+                )}
               </div>
-            )}
+            </div>
           </div>
-        </div>
-      </div>
-
-      <div className="col-md-7">
-        <Link to={`/detail/${id}`}>
-          <div className="text-start">
-            {category === "Путешевствия" && (
-              <h6 className="category catg-color bgc4287f5">{category}</h6>
-            )}
-            {category === "Технологии" && (
-              <h6 className="category catg-color bgcc43421">{category}</h6>
-            )}
-            {category === "Новости" && (
-              <h6 className="category catg-color bgc32CD32">{category}</h6>
-            )}
-            {category === "Бизнес" && (
-              <h6 className="category catg-color bgcc808000">{category}</h6>
-            )}
-
-            {category !== "Путешевствия" &&
-              category !== "Технологии" &&
-              category !== "Бизнес" &&
-              category !== "Новости" && (
-                <h6 className="category catg-color bgc000000">{category}</h6>
+          <div className="col-md-7">
+            <div className="text-start">
+              {category === "Путешевствия" && (
+                <h6 className="category catg-color bgc4287f5">{category}</h6>
+              )}
+              {category === "Технологии" && (
+                <h6 className="category catg-color bgcc43421">{category}</h6>
+              )}
+              {category === "Новости" && (
+                <h6 className="category catg-color bgc32CD32">{category}</h6>
+              )}
+              {category === "Бизнес" && (
+                <h6 className="category catg-color bgcc808000">{category}</h6>
               )}
 
-            <span className="title py-2">{title}</span>
-            <span className="meta-info">
-              <p className="author">{author}</p> -&nbsp;
-              {timestamp.toDate().toDateString()}
-            </span>
-          </div>
-          <div className="short-description text-start">
-            <ReactMarkdown children={description} />
-          </div>
-        </Link>
+              {category !== "Путешевствия" &&
+                category !== "Технологии" &&
+                category !== "Бизнес" &&
+                category !== "Новости" && (
+                  <h6 className="category catg-color bgc000000">{category}</h6>
+                )}
 
-        {user && user.uid === userId && (
-          <div style={{ float: "right" }}>
-            <FontAwesome
-              name="trash"
-              style={{ margin: "5px", cursor: "pointer" }}
-              size="2x"
-              onClick={() => handleDelete(id)}
-            />
+              <span className="title py-2">{title}</span>
+              <span className="meta-info">
+                <p className="author">{author}</p> -&nbsp;
+                {timestamp.toDate().toDateString()}
+              </span>
+            </div>
+            <div className="short-description text-start">
+              <ReactMarkdown children={description} />
+            </div>
 
-            <Link to={`/update/${id}`}>
-              <FontAwesome
-                name="edit"
-                style={{ cursor: "pointer" }}
-                size="2x"
-              />
-            </Link>
-          </div>
-        )}
-      </div>
-    </div>
+            {user && user.uid === userId && (
+              <div style={{ float: "right" }}>
+                <FontAwesome
+                  name="trash"
+                  style={{ margin: "5px", cursor: "pointer" }}
+                  size="2x"
+                  onClick={() => handleDelete(id)}
+                />
+
+                <Link to={`/update/${id}`}>
+                  <FontAwesome
+                    name="edit"
+                    style={{ cursor: "pointer" }}
+                    size="2x"
+                  />
+                </Link>
+              </div>
+            )}
+          </div>{" "}
+        </div>{" "}
+      </Link>
+    </>
   );
 };
 
